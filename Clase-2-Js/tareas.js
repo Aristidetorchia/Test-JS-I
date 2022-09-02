e7eb04ed7d979c951ce1e62ad77e273cc9dae2a7
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-02 05:27:12 -0300

    linux ghhbbg

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -171,6 +171,7 @@ function deEuroAdolar(euro) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto * ancho;
 }
 
 function retornarPerimetro(lado) {
