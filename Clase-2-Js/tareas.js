8a851374b8b24a9a4a3f68c49ad2f11277772f5c
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-10 02:07:32 -0300

    windowsi

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -160,7 +160,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return ;
+  return (str + "!") ;
 }
 
 function combinarNombres(nombre, apellido) {
