26b61446879535fafc4f634219c9245432f2aa4d
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-10 22:14:05 -0300

    windowsi

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -82,10 +82,15 @@ function menosQueNoventa(num = 89) {
   }
   }
 
-function mayorQueCincuenta(num) {
+function mayorQueCincuenta(num = 51) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num > 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function obtenerResto(x, y) {
