fd4a1249b0456ee145c5e77bbdf58773d5126fc4
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-10 23:26:17 -0300

    LINUX MANJARO

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -223,7 +223,7 @@ function esVocal(letra) {
   if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
     return "Es vocal";
   } else if (letra.length !== 1) {
-    return "Dato incorrecto";
+    return Dato incorrecto;
   }
 }
 
