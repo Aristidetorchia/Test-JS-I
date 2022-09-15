4812251401c6a14f10d2fb1f3cee012f8b5412a0
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-15 04:09:14 -0300

    manjaro

--- Clase-3-Js/tareas.js
+++ Clase-3-Js/tareas.js
@@ -99,6 +99,16 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   // Tu código:
+  if (numero === 2){
+    return true;
+  }else if(numero < 2){
+  return false;
+  }for(var i = 2 ; i < numero;i++){
+    if(numero % i === 0){
+      return false;
+    }
+  }
+  return true;
 }
 
 function doWhile(numero) {
