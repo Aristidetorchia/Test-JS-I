cb98ad9586fcff57405b99d1c375f2d053ec4885
Aristidetorchia <corsotorkia999@gmail.com>
2022-09-15 18:57:47 -0300

    linux 5

--- Clase-3-Js/tareas.js
+++ Clase-3-Js/tareas.js
@@ -79,6 +79,13 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( 20 < numero && 50 > numero){
+    return true;
+  }else{
+    return false;
+  }
+
+
 }
 
 function esEntero(numero) {
